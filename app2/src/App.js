29fb9a23c695b5653f4c6090bc59b9b8951c8ae0
Ganesh Rathod <ganeshrat1994@gmail.com>
2024-10-15 09:38:56 +0530

    second commit

--- app2/src/App.js
+++ app2/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload. this is react app
+          Edit <code>src/App.js</code> and save to reload. this is react app branch
         </p>
         <a
           className="App-link"
